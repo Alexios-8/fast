a3d4baa6d7e0936a01fd4c33b815613ea287f078
Alexios-8 <92685810+Alexios-8@users.noreply.github.com>
2022-06-07 18:00:31 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+player1name = document.getElementById("player1_name_input").value;
+player2name = document.getElementById("player2_name_input").value;
+
+function addUser(){
+    localStorage.setItem("Player 1 Name", player1name);
+    localStorage.setItem("Player 2 Name", player2name);
+}
